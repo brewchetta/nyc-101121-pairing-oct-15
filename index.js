b080320e156c7a0e84d0b6dd929256b447cee6a3
brewchetta <chett.tiller@gmail.com>
2021-10-14 15:46:11 -0400

    oct 15th

---
+++ index.js
@@ -0,0 +1,57 @@
+const movies = [
+  {
+    title: "The Shining",
+    cast: [
+      "Jack Nicholson",
+      "Shelley Duval",
+      "Danny Lloyd",
+      "Scatman Crothers"
+    ]
+    runtime: 146,
+    year: 1980
+  },
+  {
+    title: "Ghostbusters",
+    cast: [
+      "Bill Murray",
+      "Dan Aykroyd",
+      "Sigourney Weaver"
+    ]
+    runtime: 105,
+    year: 1984
+  },
+  {
+    title: "Alien",
+    cast: [
+      "Sigourney Weaver",
+      "Ian Holme"
+    ]
+    runtime: 117,
+    year: 1979
+  },
+  {
+    title: "Predator 2",
+    cast: [
+      "Danny Glover",
+      "Gary Busey",
+      "Kevin Peter Hall"
+    ]
+    runtime: 108,
+    year: 1990
+  },
+]
+
+// Challenge One
+// Create a new array with only the movie titles. You can use one of these methods to do it: `.filter`, `.map`, `.reduce`, `.sort`. Which one did you use and why?
+
+// Challenge Two
+// Use `.find` in order to find the first movie made after 1980. Use `.filter` to find every movie made after 1980. What's the difference between `.find` and `.filter`?
+
+// Challenge Three
+// Use `.filter` to get an array of movies where Sigourney Weaver was a cast member.
+
+// BONUS
+// `.sort` the movies by year so they begin with the earliest year and end with the latest.
+
+// BONUS
+// Write code that finds the average runtime for the movies.
